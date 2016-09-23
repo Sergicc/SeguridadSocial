8e0ef41fa6c7aa0d1a268284f5f5e7b7c51ffdd7
Sergicc <sergicortes.c@gmail.com>
2016-09-23 18:07:18 +0200

    a

--- src/SeguridadSocial.java
+++ src/SeguridadSocial.java
@@ -105,7 +105,15 @@ public class SeguridadSocial {
 
     public List<Persona> obtenerTodas(){
 
+     /*   List<Persona> aux = new ArrayList<>();
+
+        for(Persona personaActual : personasList){
+            aux.add(personaActual);
+        }
+        return aux;
+        */
         return personasList;
+        //?
     }
 
     @Override
